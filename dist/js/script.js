799eac395c41e3de8b1656217e982250426374c3
jackseparoan <muhammadmuzaki119@gmail.com>
2023-11-15 08:58:15 +0700

    commit awal web portofolio zaki

---
+++ dist/js/script.js
@@ -0,0 +1,56 @@
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const keAtas = document.querySelector('#keatas');
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        keAtas.classList.remove('hidden');
+        keAtas.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        keAtas.classList.remove('flex');
+        keAtas.classList.add('hidden');
+    }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+
+//ngilangin menu pake klik diluar menu hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//togel mode gelap
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+
+    }
+});
+
+//menyesuaikan posisi toggle pada setiap mode yang sedang dipilih
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
